f9b4040ca864f699f5d374f3c9185fc3f181e0b8
Pavel <rossohinpaveldev@gmail.com>
2024-12-28 11:04:58 +0300

    fixes

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def ping():
     return {'Message': 'Pong'}
 
 
-@app.patch('/rename/', status_code=200, tags=['Main'])
+@app.get('/rename/', status_code=200, tags=['Main'])
 async def main(deal_id: int):
     """Переименовывает продукты в сделке"""
     products = await rename_products(deal_id)
