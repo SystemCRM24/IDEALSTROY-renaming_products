cef14ce428d4c01745dcc47cd1b8ec21adcb7f87
Pavel <rossohinpaveldev@gmail.com>
2025-01-17 13:19:13 +0300

    test

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ async def delete(sp_id: str) -> list:
     Удаляет продукт Бытовка из продуктов в сделке, если есть продукт прокат.
     sp_id - ид смарт процесса. Он приходит в формате SI_2827, поэтому, парсим его как строку.
     """
+    print(sp_id)
     if sp_id.startswith('SI_'):
         sp_id = sp_id[3:]
     return await delete_cabin(sp_id)
