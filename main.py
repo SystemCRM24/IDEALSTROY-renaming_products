925c6e92b836fdd7b414624573a1a0e6d634a741
Pavel <rossohinpaveldev@gmail.com>
2025-01-17 13:21:31 +0300

    test1

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ async def rename(deal_id: int) -> list:
 
 
 @app.post('/delete/', status_code=200, tags=['Main'])
-async def delete(sp_id: str) -> list:
+async def delete(sp_id: str, product: str) -> list:
     """
     Удаляет продукт Бытовка из продуктов в сделке, если есть продукт прокат.
     sp_id - ид смарт процесса. Он приходит в формате SI_2827, поэтому, парсим его как строку.
     """
-    print(sp_id)
+    print(sp_id, product)
     if sp_id.startswith('SI_'):
         sp_id = sp_id[3:]
     return await delete_cabin(sp_id)
